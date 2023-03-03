195fa8c1822a337846858cce5e2102b39379229c
rira <101010hayakawa@gmail.com>
2023-03-03 18:31:23 +0900

    ユーザーの情報を追加
    ユーザーの持つ情報に郵便番号、住所、自己紹介文を追加しました。
    users/editで追加した情報を編集できるようにしました。
    usersでユーザー一覧を見れるようにしました。
    users/:idでユーザーの情報を見れるようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_account_update_params, if: :devise_controller?
+
+  protected
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zip_code, :address, :introduction])
+  end
 end
