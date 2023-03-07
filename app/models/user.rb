deb5e3e87190ec397b2dd8cb24d08dc3c4287061
rira <101010hayakawa@gmail.com>
2023-03-07 13:38:25 +0900

    アバターの拡張子を限定
    アバターに登録できるファイルの拡張子をjpg,jpeg,png,gifに限定しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  validate :avatar_format
+  def avatar_format
+    return unless avatar.attached?
+
+    allowed_types = %w[.jpg .jpeg .png .gif]
+    unless allowed_types.include?(avatar.blob.content_type)
+      errors.add(:avatar, 'はjpeg、jpg、png、gif形式のみ許可されています')
+    end
+  end
 end
