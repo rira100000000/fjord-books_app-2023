25cfcc2f0f48ac2a319de9ee88d8cee024c50860
rira <101010hayakawa@gmail.com>
2023-03-07 14:08:37 +0900

    バグ修正
    アバターの拡張子設定が間違っていたため修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   def avatar_format
     return unless avatar.attached?
 
-    allowed_types = %w[.jpg .jpeg .png .gif]
-    unless allowed_types.include?(avatar.blob.content_type)
+    allowed_types = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+
+    unless avatar.blob.content_type.in?(allowed_types)
       errors.add(:avatar, 'はjpeg、jpg、png、gif形式のみ許可されています')
     end
   end
