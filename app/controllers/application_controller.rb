075a51d4deea648f17156834d2fbcc1108eeec0e
rira <101010hayakawa@gmail.com>
2023-03-07 12:30:36 +0900

    ユーザーのアバター表示機能追加
    ユーザーモデルにアバターを追加しました。
    ユーザー一覧画面、詳細画面で表示されるようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     keys = %i[name postal_code address self_introduction]
-    devise_parameter_sanitizer.permit(:sign_up, keys:)
-    devise_parameter_sanitizer.permit(:account_update, keys:)
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar name postal_code address self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar name postal_code address self_introduction])
   end
 
   private
