d7f6cc7a03f419bf7380b9a567c150d2242519aa
rira <101010hayakawa@gmail.com>
2023-03-10 11:25:24 +0900

    翻訳にパラメータを使用するよう修正
    パラメータ化されていなかったため修正しました。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('controller.notice.success_create') }
+        format.html { redirect_to book_url(@book), notice: t('controller.notice.success_create', name: @book.model_name.human) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('controller.notice.success_edit') }
+        format.html { redirect_to book_url(@book), notice: t('controller.notice.success_edit', name: @book.model_name.human) }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('controller.notice.success_destroy') }
+      format.html { redirect_to books_url, notice: t('controller.notice.success_destroy', name: @book.model_name.human) }
       format.json { head :no_content }
     end
   end
