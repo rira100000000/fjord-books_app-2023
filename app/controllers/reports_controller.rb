6d12d146d05154ddae091a772e82de2adca90d21
rira <101010hayakawa@gmail.com>
2023-03-12 14:28:17 +0900

    Reportの更新時にメンションも更新する
    Reportが更新されたとき、mentionも更新されるようにしました。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,8 +24,8 @@ class ReportsController < ApplicationController
 
   def create
     @report = current_user.reports.new(report_params)
-    @mention_id_list = @report.content.scan(%r{(?<=http://localhost:3000/reports/)\d+})
-    if @mention_id_list.empty?
+    mention_id_list = @report.content.scan(%r{(?<=http://localhost:3000/reports/)\d+})
+    if mention_id_list.empty?
       if @report.save
         redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
       else
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
     else
       result = @report.transaction do
         @report.save!
-        create_mentions(@mention_id_list)
+        create_mentions(mention_id_list)
       end
       if result
         redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
@@ -45,10 +45,25 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.update(report_params)
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    mentioning_list = @report.mentioning_reports
+    mention_id_list = report_params[:content].scan(%r{(?<=http://localhost:3000/reports/)\d+})
+    if mentioning_list.empty? && mention_id_list.empty?
+      if @report.update(report_params)
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+      else
+        render :edit, status: :unprocessable_entity
+      end
     else
-      render :edit, status: :unprocessable_entity
+      result = @report.transaction do
+        @report.update(report_params)
+        destroy_mentions(mentioning_list)
+        create_mentions(mention_id_list)
+      end
+      if result
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
   end
 
@@ -70,7 +85,11 @@ class ReportsController < ApplicationController
 
   def create_mentions(mention_id_list)
     mention_id_list.uniq.each do |mention_id|
-      Mention.create(mention_report: @report, mentioned_report: Report.find(mention_id.to_i))
+      Mention.create(mention_report: @report, mentioned_report: Report.find(mention_id.to_i)) unless mention_id == @report.id
     end
+  end
+
+  def destroy_mentions(mentioning_list)
+    mentioning_list.each(&:destroy)
   end
 end
