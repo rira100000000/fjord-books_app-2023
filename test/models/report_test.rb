d70ae4ddc2b73142a42315a5f611ebc58bd50fda
rira <101010hayakawa@gmail.com>
2023-03-17 17:10:52 +0900

    テストの修正、追加
    単体テストが動くように修正しました。
    システムテストを追加しました。
    fixtureを修正しました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @report = reports(:one)
+  end
+
+  test '日報作成者が現在ログインしているユーザーであれば編集可能であること' do
+    @user = users(:one)
+    sign_in @user
+    assert_equal true, @report.editable?(@user)
+  end
+
+  test '日報作成者が現在ログインしているユーザーでなければ編集不可能であること' do
+    @user = users(:two)
+    sign_in @user
+
+    assert_equal false, @report.editable?(@user)
+  end
 end
