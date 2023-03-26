8ca85292ebd11cb3fecd665fbfc2551372a95ce4
rira <101010hayakawa@gmail.com>
2023-03-26 18:48:34 +0900

    テストで画面上の表示を確認するよう修正
    book_testの各テストで表示される内容が正しいかを確認するようにしました。
    fixtureの内容をより現実的なものにしました。

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,13 +4,19 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:book1)
+    @raise_server_errors = Capybara.raise_server_errors
+
     @user = users(:user_taro)
     visit root_path
     fill_in 'Eメール', with: 'user1@example.com'
     fill_in 'パスワード', with: 'password'
     click_on 'ログイン'
     assert_selector 'h1', text: '本の一覧'
+  end
+
+  teardown do
+    # 元の値に戻す
+    Capybara.raise_server_errors = @raise_server_errors
   end
 
   test 'visiting the index' do
@@ -22,30 +28,43 @@ class BooksTest < ApplicationSystemTestCase
     visit books_url
     click_on '本の新規作成'
 
-    fill_in 'タイトル', with: 'TEST_TITLE'
-    fill_in 'メモ', with: 'TEST_MEMO'
+    fill_in 'タイトル', with: 'オブジェクト指向でなぜつくるのか'
+    fill_in 'メモ', with: 'オブジェクト指向と開発技術の本質をズバリ解説'
     click_on '登録する'
 
     assert_text '本が作成されました。'
+    assert_selector 'p', text: 'オブジェクト指向でなぜつくるのか'
+    assert_selector 'p', text: 'オブジェクト指向と開発技術の本質をズバリ解説'
     click_on '本の一覧に戻る'
   end
 
   test 'should update Book' do
-    visit book_url(@book)
+    visit book_url(books(:book1))
+    assert_selector 'p', text: 'テストに関する本'
+    assert_selector 'p', text: 'ロボットの絵が描かれている'
     click_on 'この本を編集'
 
-    fill_in 'タイトル', with: 'EDIT_TITLE'
-    fill_in 'メモ', with: 'EDIT_MEMO'
+    fill_in 'タイトル', with: '初めての自動テスト'
+    fill_in 'メモ', with: 'Webシステムのための自動テスト基礎'
     click_on '更新'
 
     assert_text '本が更新されました。'
+    assert_selector 'p', text: '初めての自動テスト'
+    assert_selector 'p', text: 'Webシステムのための自動テスト基礎'
+
     click_on '本の一覧に戻る'
   end
 
   test 'should destroy Book' do
-    visit book_url(@book)
+    book = books(:book2)
+    visit book_url(book)
     click_on 'この本を削除', match: :first
 
     assert_text '本が削除されました。'
+
+    Capybara.raise_server_errors = false
+
+    visit book_url(book)
+    assert_text 'ActiveRecord::RecordNotFound'
   end
 end
