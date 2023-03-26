7ea28090ee7e2949bc6a19c3807c5b65c05fe501
rira <101010hayakawa@gmail.com>
2023-03-26 16:21:23 +0900

    driven_byの不要なオプションを削除
    driven_byに必要のないオプションを指定していたため、削除しました。

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,5 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |options|
-    options.add_argument('--no-sandbox')
-  end
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
