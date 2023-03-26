7fddde1847ddecb6f524572a13809562ba217334
rira <101010hayakawa@gmail.com>
2023-03-26 17:32:12 +0900

    不要なログイン処理の削除
    modelのテストでログイン処理は必要なかったため、削除しました。
    インスタンス変数にする必要のない変数をローカル変数に修正しました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,24 +5,23 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   include Devise::Test::IntegrationHelpers
 
-  setup do
-    @report = reports(:report1)
+  test '日報作成者は作成した日報を編集可能であること' do
+    report = reports(:report1)
+    user = users(:user_taro)
+
+    assert_equal true, report.editable?(user)
   end
 
-  test '日報作成者が現在ログインしているユーザーであれば編集可能であること' do
-    @user = users(:user_taro)
-    sign_in @user
-    assert_equal true, @report.editable?(@user)
-  end
+  test '日報作成者以外は日報を編集不可能であること' do
+    report = reports(:report1)
+    user = users(:user_nameless)
 
-  test '日報作成者が現在ログインしているユーザーでなければ編集不可能であること' do
-    @user = users(:user_nameless)
-    sign_in @user
-
-    assert_equal false, @report.editable?(@user)
+    assert_equal false, report.editable?(user)
   end
 
   test '日報の作成日をdate型で取得できること' do
-    assert_equal Time.zone.today, @report.created_on
+    report = reports(:report1)
+
+    assert_equal Time.zone.today, report.created_on
   end
 end
